f5d9f19defda4bfb77f9989f520030d34e0f636f
athena272 <guilhermera272@gmail.com>
2025-06-22 20:37:34 -0300

    Create gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const concat = require('gulp-concat');
+const minifyCSS = require('gulp-clean-css');
+const terser = require('gulp-terser');
+
+const paths = {
+    scss: {
+        src: './assets/scss/main.scss',
+        dest: './assets/scss',
+        watch: './assets/scss/**/*.scss'
+    },
+    js: {
+        src: './assets/js/main.js',
+        dest: './assets/dist/js',
+        watch: './assets/js/**/*.js'
+    }
+};
+
+function compileScss() {
+    return gulp.src(paths.scss.src)
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest(paths.scss.dest));
+}
+
+function minifyJs() {
+    return gulp.src(paths.js.src)
+        .pipe(terser())
+        .pipe(concat('main.min.js'))
+        .pipe(gulp.dest(paths.js.dest));
+}
+
+function watchFiles() {
+    gulp.watch(paths.scss.watch, compileScss);
+    gulp.watch(paths.js.watch, minifyJs);
+}
+
+const build = gulp.parallel(compileScss, minifyJs);
+const watch = watchFiles;
+
+exports.build = build;
+exports.watch = watch;
+exports.default = build; 
